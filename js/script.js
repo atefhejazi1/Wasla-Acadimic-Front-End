3a73bb373a164a3db07644d628c12ab154694c48
unknown <atefhejazi10@gmail.com>
2021-12-03 15:33:46 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// let header = document.querySelector("header");
+
+// let arrOfImages = ["header.jpg", "header2.jpg", "header3.jpg"];
+// header.style.backgroundImage = 'url("../images/header/header.jpg")';
+// setInterval(() => {
+//   let random = Math.floor(Math.random() * arrOfImages.length);
+//   header.style.backgroundImage = `url("../images/header/${arrOfImages[random]}")`;
+// }, 3000);
